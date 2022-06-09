593feb8c6c584f32818a60dd2cf50ed308885eb6
Bautista Ortiz Aury <bauti707@gmail.com>
2022-06-09 13:55:07 -0300

    Cambios con Gus

--- index.js
+++ index.js
@@ -28,32 +28,62 @@ let nombre = prompt("Ingrese su nombre")
 console.log ("Hola " + nombre)*/
 
 let BotellonXl={
+    Nombre: "BotellonXL",
     Litros: "1,9L",
     Precio: "$980",
-    Recarga: "$500"
 }
 let BotellonM={
+    Nombre: "BotellonM",
     Litros: "1L",
     Precio: "$600",
     Recarga: "$300"
 }
 let BotellaXl={
+    Nombre: "Botellaxl",
     Litros: "2L",
     Precio: "$850"
 }
 let BotellaM={
+    Nombre: "BotellaM",
     Litros: "1L",
     Precio: "$550"
 }
 let BotellaS={
+    Nombre:"BotellaS",
     Litros: "500ml",
     Precio: "$300"
 }
 let Pinta={
+    Nombre: "Honey",
     Litros: "750ml",
     Precio: "$270"
 }
 
-let Ingresanombre= prompt("Ingrese su nombre")
+let Ingresanombre= prompt("Ingrese su nombre o salir para finalizar")
 alert("Hola " + Ingresanombre);
-let producto=parseInt (prompt("Ingrese los productos que valla a comprar"))
+let producto=(prompt("Ingrese el productos que valla a comprar"+ "\n" +
++"1 " + Pinta.Nombre +"\n" + "2 " +BotellonXl.Nombre +"\n" +"3 "+ BotellaM.Nombre+ "o ingrese salir para finalizar"))
+ 
+while (producto !== "salir"){
+    let Preciototal = ""
+    if (producto === "1"|| producto==="2" || producto==="3"){
+        switch (producto){
+            case "1":
+            alert("El precio del producto es de "+Pinta.Precio)
+            Preciototal += Pinta.Precio
+            break
+
+        }
+         producto=(prompt("Ingrese el productos que valla a comprar"+ "\n" +
++"1 " + Pinta.Nombre +"\n" + "2 " +BotellonXl.Nombre +"\n" +"3 "+ BotellaM.Nombre+ "o ingrese salir para finalizar"))
+      
+    }
+    else {
+        alert ("Usted no ingreso una opcion valida")
+        producto=(prompt("Ingrese el productos que valla a comprar"+ "\n" +
++"1 " + Pinta.Nombre +"\n" + "2 " +BotellonXl.Nombre +"\n" +"3 "+ BotellaM.Nombre+ "o ingrese salir para finalizar"))
+    }
+
+
+    
+}
